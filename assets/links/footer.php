4bbfd65ee4d7e99afe801b6ecd844db0eaccb2e5
Ghel12 <angelmaecarpeso@gmail.com>
2024-05-02 11:02:08 +0800

    admission Guide sidebar

---
+++ assets/links/footer.php
@@ -0,0 +1,78 @@
+<!-- ======= Footer ======= -->
+<footer id="footer">
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-2 col-md-6">
+          <div class="footer-info">
+            <!-- First column content -->
+            <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="Small Logo" class="img-fluid small-logo"></a>
+            <div class="social-links mt-3 d-flex align-items-center justify-content-center">
+    <a href="#" class="twitter me-2"><i class="bx bxl-twitter"></i></a>
+    <a href="#" class="facebook me-2"><i class="bx bxl-facebook"></i></a>
+    <a href="#" class="instagram me-2"><i class="bx bxl-instagram"></i></a>
+    <a href="#" class="google-plus me-2"><i class="bx bxl-skype"></i></a>
+    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+</div>
+
+          </div>
+        </div>
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <!-- Second column content -->
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Departments Services</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Contacts</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4>Our Services</h4>
+          <ul>
+            <!-- Third column content -->
+            <li><a href="#">Price List</a></li>
+            <li><a href="#">Department Item</a></li>
+            <li><a href="#">Room Rates</a></li>
+            <li><a href="#">Philhealth Case Rate Packages<span class="align-text-bottom"></span></a></li>
+            <li><a href="#">Investment</a></li>
+          </ul>
+
+        </div>
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4>Other Links</h4>
+          <ul>
+            <!-- Fourth column content -->
+            <li><a href="#">Job Announcements</a></li>
+            <li><a href="#">Equipment Acquisition</a></li>
+            <li><a href="#">Accredited HMOS</a></li>
+            <li><a href="#">Packages For You</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-4 col-md-6 footer-links">
+          <h4>Contact Us</h4>
+          <ul>
+  <!-- Fifth column content -->
+            <li><i class="bi bi-geo-alt"></i> Block 8, Lot 1A & 1B, Dewey Ave. | Maps & Directions CBD Area, Subic Bay Freeport Zone, Philippines</li>
+            <li><i class="bi bi-telephone"></i> Phone: (047) - 250 - 6070 Local 700</li>
+            <li><i class="bi bi-envelope"></i> Email: baypointehospitalmedicalcenter@yahoo.com</li>
+            <li><i class="bi bi-facebook"></i> Facebook: Allied Care Experts Medical Center - Baypoint</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Batang COMTEQ</span></strong>. All Rights Reserved
+      </div>
+      <!-- <div class="credits"> -->
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/medicio-free-bootstrap-theme/ -->
+        <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div> -->
+    </div>
+  </footer><!-- End Footer -->
--- index.php
+++ index.php
@@ -48,11 +48,11 @@
   <!-- Main -->
 
   <main>
-  <div class="bg-dark py-5 my-5" id="wwa" style="background-image: url('assets/img/slide/slide-1.png'); background-size: cover; background-position: center; filter: brightness(50%)">
+  <div class="bg-dark py-5 my-5" id="wwa" style="background-image: url('assets/img/slide/slide-1.png'); background-size: cover; background-position: center; filter: brightness(50%); height: 50%;">
     <div class="py-5">
     </div>
     <div class="container">
-        <div class="col-lg-6" style="height: 50%;">
+        <div class="col-lg-6">
             <div class="">
             <h2>Welcome to <span>Ace Medical Center Baypointe</span></h2>
             <p>Baypointe Hospital and Medical Center is a member of the ACE (Allied Care Experts) Group of Hospitals
@@ -918,65 +918,81 @@
 
   <!-- ======= Footer ======= -->
   <footer id="footer">
-    <div class="footer-top">
-      <div class="container">
-        <div class="row">
-
-          <div class="col-lg-3 col-md-6">
-            <div class="footer-info">
-              <h3>Medicio</h3>
-              <p>
-                A108 Adam Street <br>
-                NY 535022, USA<br><br>
-                <strong>Phone:</strong> +1 5589 55488 55<br>
-                <strong>Email:</strong> info@example.com<br>
-              </p>
-              <div class="social-links mt-3">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-2 col-md-6 footer-links">
-            <h4>Useful Links</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Home</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">About us</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Services</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Terms of service</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Privacy policy</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-3 col-md-6 footer-links">
-            <h4>Our Services</h4>
-            <ul>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Design</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Web Development</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Product Management</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Marketing</a></li>
-              <li><i class="bx bx-chevron-right"></i> <a href="#">Graphic Design</a></li>
-            </ul>
-          </div>
-
-          <div class="col-lg-4 col-md-6 footer-newsletter">
-            <h4>Our Newsletter</h4>
-            <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
-            <form action="" method="post">
-              <input type="email" name="email"><input type="submit" value="Subscribe">
-            </form>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-2 col-md-6">
+          <div class="footer-info">
+            <!-- First column content -->
+            <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="Small Logo" class="img-fluid small-logo"></a>
+            <div class="social-links mt-3 d-flex align-items-center justify-content-center">
+    <a href="#" class="twitter me-2"><i class="bx bxl-twitter"></i></a>
+    <a href="#" class="facebook me-2"><i class="bx bxl-facebook"></i></a>
+    <a href="#" class="instagram me-2"><i class="bx bxl-instagram"></i></a>
+    <a href="#" class="google-plus me-2"><i class="bx bxl-skype"></i></a>
+    <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+</div>
 
           </div>
+        </div>
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4>Useful Links</h4>
+          <ul>
+            <!-- Second column content -->
+              <li><a href="#">Home</a></li>
+              <li><a href="#">About us</a></li>
+              <li><a href="#">Departments Services</a></li>
+              <li><a href="#">News</a></li>
+              <li><a href="#">Contacts</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4>Our Services</h4>
+          <ul>
+            <!-- Third column content -->
+            <li><a href="#">Price List</a></li>
+            <li><a href="#">Department Item</a></li>
+            <li><a href="#">Room Rates</a></li>
+            <li><a href="#">Philhealth Case Rate Packages<span class="align-text-bottom"></span></a></li>
+            <li><a href="#">Investment</a></li>
+          </ul>
 
+        </div>
+        <div class="col-lg-2 col-md-6 footer-links">
+          <h4>Other Links</h4>
+          <ul>
+            <!-- Fourth column content -->
+            <li><a href="#">Job Announcements</a></li>
+            <li><a href="#">Equipment Acquisition</a></li>
+            <li><a href="#">Accredited HMOS</a></li>
+            <li><a href="#">Packages For You</a></li>
+          </ul>
+        </div>
+        <div class="col-lg-4 col-md-6 footer-links">
+          <h4>Contact Us</h4>
+          <ul>
+  <!-- Fifth column content -->
+            <li><i class="bi bi-geo-alt"></i> Block 8, Lot 1A & 1B, Dewey Ave. | Maps & Directions CBD Area, Subic Bay Freeport Zone, Philippines</li>
+            <li><i class="bi bi-telephone"></i> Phone: (047) - 250 - 6070 Local 700</li>
+            <li><i class="bi bi-envelope"></i> Email: baypointehospitalmedicalcenter@yahoo.com</li>
+            <li><i class="bi bi-facebook"></i> Facebook: Allied Care Experts Medical Center - Baypoint</li>
+          </ul>
         </div>
       </div>
     </div>
-
-
+  </div>
+  <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Batang COMTEQ</span></strong>. All Rights Reserved
+      </div>
+      <!-- <div class="credits"> -->
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/medicio-free-bootstrap-theme/ -->
+        <!-- Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+      </div> -->
+    </div>
   </footer><!-- End Footer -->
 
   <div id="preloader"></div>
